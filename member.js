46697a7fae6ebfb87942134b4e6f9a91706949ca
Amberajsingh <128460208+Amberajsingh@users.noreply.github.com>
2025-02-25 12:45:45 +0000

    second file commit

--- member.js
+++ member.js
@@ -1,3 +1,16 @@
-function skillMember(){
-    return calculateNumbers(1, 2);
-}
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsClose = document.getElementById("memberSkillsClose");
+    var memberSkillsContent = document.getElementById("memberSkillsContent");
+  
+    memberSkillsContent.innerHTML = skills.innerHTML;
+    member.style.display = "none";
+    memberSkills.style.display = "block";
+  
+    memberSkillsClose.onclick = function() {
+      member.style.display = "block";
+      memberSkills.style.display = "none";
+    }
+  }
